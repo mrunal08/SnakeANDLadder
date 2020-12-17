0f20085234d349dcaeb96320f818abb2ca18dde1
Mrunal_Gadhave <gadhavemrunal@gmail.com>
2020-12-17 11:03:03 +0530

    USE CASE 2-ROlling the die

---
+++ U2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+snakeLadder
+
+function snakeLadder(){
+   echo "Welcome to Snake And ladder game"
+   position=0;
+   echo "Player added at " $position1 "position"
+   rollDice
+}
+
+function rollDice(){
+   dice=$((RANDOM%6+1))
+   echo $dice
+}
+
